46fb79070623044b39c8942c121fce1579bd5fa3
otmichael <ozmichaelt@icloud.com>
2019-11-16 14:45:11 -0800

    New version test

--- js/main.js
+++ js/main.js
@@ -6,12 +6,25 @@ $(document).ready(function() {
 		fixedContentPos: true,
 		mainClass: 'mfp-no-margins mfp-with-zoom',
 		image: {
-			verticalFit: true
+			verticalFit: true,
 		},
 		zoom: {
 			enabled: true,
-			duration: 300
-		}
-	});
+			duration: 300,
+		},
+	})
 
-});
+	window.onscroll = function() {
+		scrollFunction()
+	}
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+			document.getElementById('tabs-container').style.padding = '0'
+			document.getElementById('logo').style.fontSize = '25px'
+		} else {
+			document.getElementById('tabs-container').style.padding = '20px 0'
+			document.getElementById('logo').style.fontSize = '35px'
+		}
+	}
+})
